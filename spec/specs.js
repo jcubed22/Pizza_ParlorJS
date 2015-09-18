fc1592f56f079aee684f1944e276c958595d2aaa
Jordan Johansen <jordanjohan@gmail.com>
2015-09-18 11:51:30 -0700

    Write first test for Pizza constructor.

--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,8 @@
-describe('leapYear', function() {
-  it("is false for a year that is not divisible by 4", function() {
-    expect(leapYear(1999)).to.equal(false);
-  });
+describe('createPizza', function() {
+    it("creates a new pizza object with given information", function() {
+        var testPizza = new Pizza(12, [pepperoni], 1);
+        expect(testPizza.size).to.equal(12);
+        expect(testPizza.toppings).to.eql([pepperoni]);
+        expect(testPizza.quantity).to.equal(1);
+    });
 });
