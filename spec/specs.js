ed7beb29328c7a3b4f5ef28fee25c5b9298bf364
Jordan Johansen <jordanjohan@gmail.com>
2015-09-18 12:30:41 -0700

    Add test for pizza cost method.

--- spec/specs.js
+++ spec/specs.js
@@ -1,8 +1,13 @@
-describe('createPizza', function() {
+describe('Pizza', function() {
     it("creates a new pizza object with given information", function() {
-        var testPizza = new Pizza(12, ["pepperoni"], 1);
-        expect(testPizza.size).to.equal(12);
+        var testPizza = new Pizza(10, ["pepperoni"], 1);
+        expect(testPizza.size).to.equal(10);
         expect(testPizza.toppings).to.eql(["pepperoni"]);
         expect(testPizza.quantity).to.equal(1);
+    });
+
+    it("adds the cost method to all pizzas", function() {
+        var testPizza = new Pizza(12, ["pepperoni", "sausage"], 2)
+        expect(testPizza.cost()).to.equal(25);
     });
 });
